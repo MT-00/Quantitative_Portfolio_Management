ed3552ffc262dcc24135c3ea3c57d9164b5692fe
ken.chen <chken4869@hotmail.com>
2020-09-28 22:57:53 +0800

    refactor: CdoPricer for task 1-3

--- CdoPricer.py
+++ CdoPricer.py
@@ -35,7 +35,7 @@ class CdoPricer:
         self.frequency = frequency
         self.risk_free_rate = risk_free_rate
         self.classA_notional = classA_notional
-        self.classB_notianal = classB_notianal
+        self.classB_notional = classB_notianal
         self.classA_coupon_rate = classA_coupon_rate
         self.classB_coupon_rate = classB_coupon_rate
 
@@ -43,6 +43,10 @@ class CdoPricer:
         self.sim_size = len(default_dates)
 
         self.sim_cashflow = []
+        self.sim_result = pd.DataFrame(index=range(self.sim_size),
+                                       columns=['classA_default', 'classB_default'])
+        self.sim_statistics = {'classA_default_rate': np.nan,
+                               'classB_default_rate': np.nan}
         self._initialize_cashflow()
 
     def _initialize_cashflow(self):
@@ -50,22 +54,27 @@ class CdoPricer:
             self.sim_cashflow.append(
                 pd.DataFrame(index=list(range(1, self.periods + 1)),
                              columns=['bond_' + str(i) for i in range(self.bond_size)] +
-                                     ['aggregated_cf', 'classA_target', 'classA_get', 'classB_target', 'classB_get',
-                                      'classC_get']
+                                     ['aggregated_cf', 'classA_required', 'classA_get', 'classB_required',
+                                      'classB_get', 'classC_get']
                              )
             )
 
-    def get_default_rate(self):
-        pass
+    def get_default_rate_parameters(self, classA_notionals):
+        classA_default_rates = []
+        for classA_notion in classA_notionals:
+            self.classA_notional = classA_notion
+            self.run()
+            classA_default_rates.append(self.sim_statistics['classA_default_rate'])
+        self.multi_default_rate = pd.DataFrame({'classA_notional': classA_notionals,
+                                               'classA_default_rate': classA_default_rates})
 
-    def get_default_rate_parameters(self):
-        # loop _get_class_cf(), _get_default_rate()
-        pass
-
-    def get_cash_flow(self):
+    def run(self):
         self._get_bond_cf()
         self._get_portfolio_cf()
-        self._get_class_cf()
+        self._get_class_required_cf()
+        self._get_class_get_cf()
+        self._get_sim_result()
+        self._get_sim_statistics()
 
     def _get_bond_cf(self):
         for sim_num in range(self.sim_size):
@@ -74,24 +83,54 @@ class CdoPricer:
                 default_period = math.ceil(self.default_dates[sim_num][bond_num] * self.frequency)
                 if default_period >= self.periods:
                     self.sim_cashflow[sim_num][bond_name] = self.face_value * self.coupon_rate / self.frequency
-                    self.sim_cashflow[sim_num][bond_name].iloc[-1] = self.sim_cashflow[sim_num][bond_name].iloc[-1] + self.face_value
+                    self.sim_cashflow[sim_num][bond_name].iloc[-1] += self.face_value
                 else:
-                    self.sim_cashflow[sim_num][bond_name][:default_period] = self.face_value * self.coupon_rate / self.frequency
-                    self.sim_cashflow[sim_num][bond_name][default_period] = self.face_value * self.lgd
+                    self.sim_cashflow[sim_num][bond_name].loc[:default_period] = self.face_value * self.coupon_rate \
+                                                                                    / self.frequency
+                    self.sim_cashflow[sim_num][bond_name].loc[default_period] = self.face_value * self.lgd
             self.sim_cashflow[sim_num] = self.sim_cashflow[sim_num].fillna(0)
 
     def _get_portfolio_cf(self):
         for sim_num in range(self.sim_size):
-            self.sim_cashflow[sim_num]['aggregated_cf'] = self.sim_cashflow[sim_num][['bond_'+str(i) for i in range(self.bond_size)]].sum(axis=1)
+            self.sim_cashflow[sim_num]['aggregated_cf'] = self.sim_cashflow[sim_num][
+                                                        ['bond_' + str(i) for i in range(self.bond_size)]].sum(axis=1)
 
+    def _get_class_required_cf(self):
+        for sim_num in range(self.sim_size):
+            self.sim_cashflow[sim_num]['classA_required'] = self.classA_notional * self.classA_coupon_rate \
+                                                            / self.frequency
+            self.sim_cashflow[sim_num]['classA_required'].iloc[-1] += self.classA_notional
+            self.sim_cashflow[sim_num]['classB_required'] = self.classB_notional * self.classB_coupon_rate \
+                                                            / self.frequency
+            self.sim_cashflow[sim_num]['classB_required'].iloc[-1] += self.classB_notional
 
-    def _get_class_cf(self):
-        pass
+    def _get_class_get_cf(self):
+        for sim_num in range(self.sim_size):
+            self.sim_cashflow[sim_num]['classA_get'] = self.sim_cashflow[sim_num][['aggregated_cf', 'classA_required']].min(axis=1)
+            self.sim_cashflow[sim_num]['cf_afterA'] = self.sim_cashflow[sim_num]['aggregated_cf'] - self.sim_cashflow[sim_num]['classA_get']
+            self.sim_cashflow[sim_num]['classB_get'] = self.sim_cashflow[sim_num][['cf_afterA', 'classB_required']].min(axis=1)
+            self.sim_cashflow[sim_num]['classC_get'] = self.sim_cashflow[sim_num]['aggregated_cf'] \
+                                                       - self.sim_cashflow[sim_num]['classA_get'] \
+                                                       - self.sim_cashflow[sim_num]['classB_get']
 
+    def _get_sim_result(self):
+        for sim_num in range(self.sim_size):
+            self.sim_result.loc[sim_num]['classA_default'] = sum(abs(self.sim_cashflow[sim_num]['classA_required']
+                                                                     - self.sim_cashflow[sim_num]['classA_get'])) != 0
+            self.sim_result.loc[sim_num]['classB_default'] = sum(abs(self.sim_cashflow[sim_num]['classB_required']
+                                                                     - self.sim_cashflow[sim_num]['classB_get'])) != 0
+
+    def _get_sim_statistics(self):
+        self.sim_statistics['classA_default_rate'] = self.sim_result['classA_default'].mean()
+        self.sim_statistics['classB_default_rate'] = self.sim_result['classB_default'].mean()
 
 pricer = CdoPricer(
-    default_dates=[[1.5, 2.2, 3.1], [7, 2.3, 3.7], [1.1, 1.9, 2.3]],
-    bond_size=3,
+    default_dates=[[1.5, 2.2, 3.1, 12, 13],
+                   [7, 7.3, 8.7, 17, 20],
+                   [1.1, 1.9, 2.3, 5, 6],
+                   [8, 1, 9, 9, 10.2],
+                   [7.1, 5.2, 6.6, 8.8, 9.9]],
+    bond_size=5,
     default_probability=0.04,
     lgd=0.6,
     face_value=10000000,
@@ -104,5 +143,7 @@ pricer = CdoPricer(
     classA_coupon_rate=0.02,
     classB_coupon_rate=0.04
 )
-pricer._get_bond_cf()
-pricer._get_portfolio_cf()
+pricer.run()
+pricer.get_default_rate_parameters(classA_notionals=list(range(10000000, 80000000, 5000000)))
+
+aa = pricer.sim_cashflow[1]
